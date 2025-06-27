08432ba6c67462e2c467975822a1e99dd7dc3c36
Sourav13here <souravsharma13here@.com>
2025-06-27 10:21:28 +0530

    Signup and login UI

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,12 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  // @ts-ignore
+    return <Stack>
+        <Stack.Screen name= "index" options={{headerShown:false}}/>
+        <Stack.Screen name= "screens/SignupScreen/SignupScreen" options={{headerShown:false}}/>
+
+    </Stack>
+
+
 }
